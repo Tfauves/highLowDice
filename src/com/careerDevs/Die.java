c65da5c515673b3441c97a3f10836f7cb31d3e88
Tfauves <tfauvelle@gmail.com>
2021-07-16 13:17:36 -0500

    Initial Commit.

---
+++ src/com/careerDevs/Die.java
@@ -0,0 +1,5 @@
+package com.careerDevs;
+
+public class Die {
+
+}
---
+++ src/com/careerDevs/Main.java
@@ -0,0 +1,8 @@
+package com.careerDevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
