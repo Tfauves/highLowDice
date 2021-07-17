cd3540e7ee430a47595570a054df4a6b662a833f
Tfauves <tfauvelle@gmail.com>
2021-07-17 17:48:51 -0500

    implemented win streak.

--- src/com/careerDevs/Main.java
+++ src/com/careerDevs/Main.java
@@ -16,12 +16,13 @@ public class Main {
             boolean activeGame = true;
             Die die1 = new Die();
             Die die2 = new Die();
+            int winStreak = 0;
 
             while (activeGame) {
-            die1.roll();
-            die2.roll();
-            int total = die1.faceUpValue + die2.faceUpValue;
-            //System.out.println(total);
+                //System.out.println("Winstreak: " + winStreak);
+                die1.roll();
+                die2.roll();
+                int total = die1.faceUpValue + die2.faceUpValue;
                 System.out.println("Will the next number be (h)igher or (l)ower than " + total);
                 String userGuess = scanner.next();
                 //System.out.println(userGuess);
@@ -31,23 +32,27 @@ public class Main {
                 System.out.println(newTotal);
 
                 if ( userGuess.equals("l") && newTotal < total) {
-
                     System.out.println("You guessed correct! You Win!");
+                    winStreak = winStreak + 1;
+
                 } else if (userGuess.equals("l") && newTotal > total){
-                    System.out.println("You Lose! Game Over!!!");
+                    System.out.println("You Lose! Game Over!!!\n" + "Your win streak: " + winStreak);
                     activeGame = false;
                 }
 
                 if ( userGuess.equals("h") && newTotal > total) {
-
                     System.out.println("You guessed correct! You Win!");
+                    winStreak = winStreak + 1;
+
                 } else if (userGuess.equals("h") && newTotal < total) {
                     System.out.println("You Lose! Game Over!!!");
+                    System.out.println("You Lose! Game Over!!!\n" + "Your win streak: " + winStreak);
                     activeGame = false;
                 }
 
                 if (newTotal == total) {
                     System.out.println("Push!!! Sorry you Lose! Game Over!!!");
+                    System.out.println("You Lose! Game Over!!!\n" + "Your win streak: " + winStreak);
                     activeGame = false;
                 }
             }
