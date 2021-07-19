cdebd38fdb6f6ae88a48b390521db451eaa8fa67
Tfauves <tfauvelle@gmail.com>
2021-07-19 11:21:30 -0400

    Corrected high low game flow and refactored push condition.

--- src/com/careerDevs/Main.java
+++ src/com/careerDevs/Main.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//        Die die = new Die();
-//        die.roll();
-//        System.out.println(die.faceUpValue);
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter (s)tart: ");
         String start = scanner.next();
@@ -17,13 +15,13 @@ public class Main {
             Die die1 = new Die();
             Die die2 = new Die();
             int winStreak = 0;
+            die1.roll();
+            die2.roll();
+            int total = die1.faceUpValue + die2.faceUpValue;
 
             while (activeGame) {
                 if (winStreak %5 == 0 && winStreak > 0)
                     System.out.println("On Fire " + winStreak + " correct guesses in a row! Keep it going.");
-                die1.roll();
-                die2.roll();
-                int total = die1.faceUpValue + die2.faceUpValue;
                 System.out.println("Will the next number be (h)igher or (l)ower than " + total);
                 String userGuess = scanner.next();
                 //System.out.println(userGuess);
@@ -32,8 +30,14 @@ public class Main {
                 int newTotal = die1.faceUpValue + die2.faceUpValue;
                 System.out.println(newTotal);
 
+                if (newTotal == total) {
+                    System.out.println("Push!!! too bad you Lose. Game Over!!!\n" + "Your win streak: " + winStreak);
+                    activeGame = false;
+                }
+
                 if ( userGuess.equals("l") && newTotal < total) {
                     System.out.println("You guessed correct! You Win!");
+                    total = newTotal;
                     winStreak = winStreak + 1;
 
                 } else if (userGuess.equals("l") && newTotal > total){
@@ -43,6 +47,7 @@ public class Main {
 
                 if ( userGuess.equals("h") && newTotal > total) {
                     System.out.println("You guessed correct! You Win!");
+                    total = newTotal;
                     winStreak = winStreak + 1;
 
                 } else if (userGuess.equals("h") && newTotal < total) {
@@ -50,10 +55,6 @@ public class Main {
                     activeGame = false;
                 }
 
-                if (newTotal == total) {
-                    System.out.println("Push!!! too bad you Lose. Game Over!!!\n" + "Your win streak: " + winStreak);
-                    activeGame = false;
-                }
             }
 
         }
